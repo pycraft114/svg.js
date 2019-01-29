065a9fedd6a991efc4c05b0aa086a752b07bc958
pycraft114 <pycraft114@gmail.com>
2019-01-29 15:15:17 +0900

    Pass generated img to loaded callback in image.js

--- src/image.js
+++ src/image.js
@@ -3,25 +3,25 @@ SVG.Image = SVG.invent({
   create: 'image'
 
   // Inherit from
-, inherit: SVG.Shape
+  , inherit: SVG.Shape
 
   // Add class methods
-, extend: {
+  , extend: {
     // (re)load image
-    load: function(url) {
+    load: function (url) {
       if (!url) return this
 
       var self = this
-        , img  = new window.Image()
-      
+        , img = new window.Image()
+
       // preload image
-      SVG.on(img, 'load', function() {
+      SVG.on(img, 'load', function () {
         SVG.off(img)
 
         var p = self.parent(SVG.Pattern)
 
-        if(p === null) return
-        
+        if (p === null) return
+
         // ensure image size
         if (self.width() == 0 && self.height() == 0)
           self.size(img.width, img.height)
@@ -29,43 +29,44 @@ SVG.Image = SVG.invent({
         // ensure pattern size if not set
         if (p && p.width() == 0 && p.height() == 0)
           p.size(self.width(), self.height())
-        
+
         // callback
         if (typeof self._loaded === 'function')
           self._loaded.call(self, {
-            width:  img.width
-          , height: img.height
-          , ratio:  img.width / img.height
-          , url:    url
+            width: img.width
+            , height: img.height
+            , ratio: img.width / img.height
+            , url: url
+            , img: img
           })
       })
 
-      SVG.on(img, 'error', function(e){
+      SVG.on(img, 'error', function (e) {
         SVG.off(img)
 
-        if (typeof self._error === 'function'){
-            self._error.call(self, e)
+        if (typeof self._error === 'function') {
+          self._error.call(self, e)
         }
       })
 
       return this.attr('href', (img.src = this.src = url), SVG.xlink)
     }
     // Add loaded callback
-  , loaded: function(loaded) {
+    , loaded: function (loaded) {
       this._loaded = loaded
       return this
     }
 
-  , error: function(error) {
+    , error: function (error) {
       this._error = error
       return this
     }
   }
-  
+
   // Add parent method
-, construct: {
+  , construct: {
     // create image element, load image and set its size
-    image: function(source, width, height) {
+    image: function (source, width, height) {
       return this.put(new SVG.Image).load(source).size(width || 0, height || width || 0)
     }
   }
